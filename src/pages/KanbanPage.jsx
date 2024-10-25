0e88650b2abd196e103eaf4c0935d51f0f0ebe9f
Diego <devgo.bydrgm21@gmail.com>
2024-10-25 09:44:16 -0300

    Se obtiene el usuario en otra página de la aplicación

--- src/pages/KanbanPage.jsx
+++ src/pages/KanbanPage.jsx
@@ -1,7 +1,11 @@
 import { TableroTareas } from "../components/TableroTareas";
 import { Header } from "../components/Header";
-
+import { useLocation } from "react-router-dom";
 export function KanbanPage() {
+
+    const location = useLocation()
+    const { token } = location.state || {}  // Acá está el token
+
     return (
 
         <div className="container mx-auto">
