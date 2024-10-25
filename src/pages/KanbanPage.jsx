66cbfe279b7cfc695ce645918a8a92cc1420033d
diego <devgo.bydrgm21@gmail.com>
2024-10-25 11:46:02 -0300

    Acceso a la api de usuarios

--- src/pages/KanbanPage.jsx
+++ src/pages/KanbanPage.jsx
@@ -1,10 +1,34 @@
 import { TableroTareas } from "../components/TableroTareas";
 import { Header } from "../components/Header";
 import { useLocation } from "react-router-dom";
+import axios from "axios";
+import { useEffect } from "react";
+import { toast } from "react-hot-toast";
+
+
 export function KanbanPage() {
 
     const location = useLocation()
-    const { token } = location.state || {}  // Acá está el token
+    const token = location.state?.token || "" // Obtener el token como cadena de texto
+
+    const getUserData = async () => {
+        try {
+            const response = await axios.get('http://127.0.0.1:8000/api/users/', {
+                headers: {
+                    Authorization: `Token ${token}`
+                }
+            })
+            console.log(response.data)
+        } catch (error) {
+            console.error(error)
+            toast.error('Algo salió mal 😢 Por favor, vuelva a iniciar sesión')
+        }
+    }
+
+    useEffect(() => {
+        getUserData()
+    }, [])
+
 
     return (
 
