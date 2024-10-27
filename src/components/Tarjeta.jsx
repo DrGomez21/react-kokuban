49e1a60461e24638dc13226d68484ddc7eb64062
diego <devgo.bydrgm21@gmail.com>
2024-10-27 15:52:32 -0300

    Mejoras de UI en Asignado.

--- src/components/Tarjeta.jsx
+++ src/components/Tarjeta.jsx
@@ -2,7 +2,7 @@
 export function Tarjeta({ title, description, tags, assignedTo }) {
     return (
         <div className="bg-[#F5FF70] border-[3px] border-[#121212] hover:shadow-[.4rem_.4rem_#121212] hover:cursor-pointer duration-150 p-4 w-full shadow-md relative">
-          <h2 className="text-lg montserrat-semibold mb-3">{title}</h2>
+          <h2 className="text-lg montserrat-semibold mb-1">{title}</h2>
           <p className="text-sm mb-4">{description}</p>
           <div className="flex flex-wrap gap-2 ">
             {tags.map((tag, index) => (
@@ -12,7 +12,7 @@ export function Tarjeta({ title, description, tags, assignedTo }) {
             ))}
           </div>
           {assignedTo && (
-            <div className="absolute top-4 right-4 bg-[#F0CA81] px-2 py-1 rounded-full text-xs montserrat-regular border border-[#121212]">
+            <div className="bg-[#F0CA81] px-2 mt-1 w-auto py-1 rounded-full text-xs montserrat-regular border border-[#121212]">
               {/* Asignado a {assignedTo} */}
               {assignedTo}
             </div>
