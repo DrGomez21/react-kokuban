eebcb9d68569975450154baea91062d5e20afa1a
Diego <devgo.bydrgm21@gmail.com>
2024-12-10 17:30:08 -0300

    Feature de filtrar por usuarios

--- src/components/NuevaSuperLista.jsx
+++ src/components/NuevaSuperLista.jsx
@@ -37,6 +37,7 @@ export function Lista({
 
   const [usernamesSpace, setUsernamesSpace] = useState([])
   const [etiquetaFiltro, setEtiquetafiltro] = useState("")
+  const [usuarioFiltro, setUsuarioFiltro] = useState("")
   const [filtrosFlag, setFiltrosFlag] = useState(false)
 
   const { register, handleSubmit } = useForm();
@@ -237,6 +238,12 @@ export function Lista({
     filtrarPorEtiqueta(true)
   }
 
+  const submitFiltroUsuario = (event) => {
+    event.preventDefault()
+    const { userFiltrar } = event.target
+    setUsuarioFiltro(userFiltrar.value)
+  }
+
   useEffect(() => {
     const tareasEnLista = tareas.filter(
       (tarea) => tarea.estado === listaId,
@@ -286,8 +293,8 @@ export function Lista({
       <div className="space-y-2">
         {tareas.map((tarea) => {
           const tarjeta = getTarea(tarea.tarjeta);
-          if (etiquetaFiltro != "") {
-            if (tarea.estado === listaId && tarjeta.etiqueta === etiquetaFiltro) {
+          if (etiquetaFiltro != "" || usuarioFiltro != "") {
+            if (tarea.estado === listaId && (tarjeta.etiqueta === etiquetaFiltro || tarjeta.asignado_a === usuarioFiltro)) {
               return (
                 <React.Fragment key={tarjeta.id}>
                   <Tarjeta
@@ -334,7 +341,7 @@ export function Lista({
 
       {/* Modal de Acciones */}
       <Modal isOpen={mostrarAcciones} onClose={() => setMostrarAcciones(false)}>
-        <div className="p-2">
+        <div className="py-2 px-4">
           <div className="flex justify-between content-center p-2 mb-4">
             <h3 className="text-lg montserrat-bold">Acciones de la Lista</h3>
             <button
@@ -500,23 +507,33 @@ export function Lista({
             )}
           </div>
 
-          <div className="mt-4">
-            <form action="">
+          {/* FILTROS DE USUARIO */}
+          <div className="mt-4 ">
+            <form onSubmit={submitFiltroUsuario} className="flex justify-between">
               <label htmlFor="option" className="p-2 montserrat-medium text-sm">Usuario:</label>
               <select
                 id="option"
+                name="userFiltrar"
                 className="px-2 py-1 bg-white border border-[#121212] rounded-md hover:cursor-pointer"
-                {...register("userFiltrar")}
               >
 
-                <option className="montserrat-regular" value={usuario.id}>{usuario.username}</option>
+                <option className="montserrat-regular" value="">Nadie...</option>
+                <option className="montserrat-regular" value={usuario.username}>{usuario.username}</option>
                 {
                   usernamesSpace.map((user) => (
-                    <option className="montserrat-regular" key={user.id} value={user.id}>{user.username}</option>
+                    <option className="montserrat-regular" key={user.id} value={user.username}>{user.username}</option>
                   ))
                 }
-
               </select>
+              <button type="submit" className="flex gap-2 content-center justify-center bg-lime-300 border border-[#121212] py-2 px-4 hover:bg-lime-400 transition duration-100 rounded-md">
+                Aplicar
+                <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-filter-search">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M11.36 20.213l-2.36 .787v-8.5l-4.48 -4.928a2 2 0 0 1 -.52 -1.345v-2.227h16v2.172a2 2 0 0 1 -.586 1.414l-4.414 4.414" />
+                  <path d="M18 18m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+                  <path d="M20.2 20.2l1.8 1.8" />
+                </svg>
+              </button>
             </form>
           </div>
         </div>
