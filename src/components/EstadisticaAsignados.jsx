510cc5c821ac9047b5d678642c7a801266aa2655
Diego <devgo.bydrgm21@gmail.com>
2024-12-11 14:29:46 -0300

    Refactor

--- src/components/EstadisticaAsignados.jsx
+++ src/components/EstadisticaAsignados.jsx
@@ -2,15 +2,6 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { PieChart, Pie, Legend, Tooltip, ResponsiveContainer } from 'recharts';
 
-const data01 = [
-  { name: 'Group A', value: 400 },
-  { name: 'Group B', value: 300 },
-  { name: 'Group C', value: 300 },
-  { name: 'Group D', value: 200 },
-  { name: 'Group E', value: 278 },
-  { name: 'Group F', value: 189 },
-];
-
 export const EstadisticaAsignados = ({ tablero, token }) => {
 
   const [data, setData] = useState([]);
